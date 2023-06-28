c0c412f1ea6e7112341b5da5a4bc0956ffdb09b8
Jiangli-w <Jiangli__W@163.com>
2023-06-28 22:47:05 +0800

    Week5 class

--- Week_5/Account.py
+++ Week_5/Account.py
@@ -1,5 +1,7 @@
 class Account:
 
+    interest = 0.02
+
     def __init__(self, account_holder):
         self.balance = 0
         self.holder = account_holder
@@ -13,3 +15,9 @@ class Account:
             return '有多少钱自己心里没点B数？'
         self.balance = self .balance - amount
         return self.balance
+
+class ChechingAccount(Account):
+    interest = 0.01
+    withdraw_fee = 1
+    def withdraw(self, amount):
+            return Account.withdraw(self, amount + self.withdraw_fee)
